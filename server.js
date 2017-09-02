676f2651a530d42d9ec95e0ee3f61172130ca8a5
Gayatri G <gayatrigopu@gmail.com>
2017-09-02 08:45:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,17 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one',function(req,res){
+  res.send("Article one requestd and will be served");  
+});
+
+app.get('/article-two',function(req,res){
+  res.send("Article two requestd and will be served");  
+});
+
+app.get('/article-three',function(req,res){
+  res.send("Article three requestd and will be served");  
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
